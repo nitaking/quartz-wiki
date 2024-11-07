2b08fee9b98d77f6237709e9a5af29d332890260
nitaking <10850034+nitaking@users.noreply.github.com>
2024-11-08 00:22:14 +0900

    Quartz sync: Nov 8, 2024, 12:22 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     },
     locale: "ja-JP",
     baseUrl: "wiki.nitaking.dev",
-    ignorePatterns: ["private", "templates", "Templates", "Excalidraw", ".obsidian"],
+    ignorePatterns: ["private", "templates", "Templates", "**/*.excalidraw.md", ".obsidian"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
